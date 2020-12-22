e7e4dd644fcb40bebcc2ad79aea6f1333ff51c99
Jonathan Palencia <strikeone88@rsthn.com>
2020-12-21 20:06:41 -0600

    - Removed built-in events from xui-element, since those are now part of Rin's root Element class. - Updated dependencies. - Published: v2.0.5

--- src/elems/xui-element.js
+++ src/elems/xui-element.js
@@ -24,16 +24,6 @@ xui.register ('xui-element',
 {
 	isRoot: true,
 
-	events: {
-		"click [data-action]": function(evt) {
-			this[evt.source.dataset.action] (evt);
-		},
-
-		"keyup(13) input[data-enter]": function(evt) {
-			this[evt.source.dataset.enter] (evt);
-		}
-	},
-
 	init: function()
 	{
 		if (this.dataset['isRoot'] == 'false')
